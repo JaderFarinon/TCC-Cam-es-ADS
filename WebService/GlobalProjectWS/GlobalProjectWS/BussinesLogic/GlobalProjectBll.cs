d763ba199d2bf86aae79ad71a272b9d6f2321e62
JaderFarinon <jadergfarinon@gmail.com>
2020-05-27 16:22:53 -0300

    Initial commit

---
+++ WebService/GlobalProjectWS/GlobalProjectWS/BussinesLogic/GlobalProjectBll.cs
@@ -0,0 +1,148 @@
+﻿using GlobalProjectWS.DataAccess;
+//using SQLFuncoes;
+using System;
+using System.Data;
+
+namespace GlobalProjectWS.BussinesLogic
+{
+    public class GlobalProjectBll
+    {
+        internal DataTable DadosUsuario(ConexaoMySql conex, string login, string senha)
+        {
+            GlobalProjectSql globalProjectSql = new GlobalProjectSql();
+            return globalProjectSql.DadosUsuario(conex, login, senha);
+        }
+
+        internal DataTable DadosUsuario(ConexaoMySql conex, string cpf)
+        {
+            GlobalProjectSql globalProjectSql = new GlobalProjectSql();
+            return globalProjectSql.DadosUsuario(conex, cpf);
+        }
+
+        internal string Login(ConexaoMySql conex, string login, string senha)
+        {
+            GlobalProjectSql globalProjectSql = new GlobalProjectSql();
+            return globalProjectSql.Login(conex, login, senha);
+        }
+
+        internal DataTable ListarProcedimentos(ConexaoMySql conex, string keyword)
+        {
+            GlobalProjectSql globalProjectSql = new GlobalProjectSql();
+            return globalProjectSql.ListarProcedimentos(conex, keyword);
+        }
+
+        internal DataTable ListarGrupos(ConexaoMySql conex)
+        {
+            GlobalProjectSql globalProjectSql = new GlobalProjectSql();
+            return globalProjectSql.ListarGrupos(conex);
+        }
+
+        internal DataTable ListarGrupos(ConexaoMySql conex, int gruId)
+        {
+            GlobalProjectSql globalProjectSql = new GlobalProjectSql();
+            return globalProjectSql.ListarGrupos(conex, gruId);
+        }
+
+        internal void ExcluirGrupo(ConexaoMySql conex, int gruId)
+        {
+            GlobalProjectSql globalProjectSql = new GlobalProjectSql();
+            globalProjectSql.ExcluirGrupo(conex, gruId);
+        }
+
+        internal DataTable ListarProcedimentos(ConexaoMySql conex, int parceiro)
+        {
+            GlobalProjectSql globalProjectSql = new GlobalProjectSql();
+            return globalProjectSql.ListarProcedimentos(conex, parceiro);
+        }
+
+        internal DataTable ListarProcedimentos(ConexaoMySql conex, int parceiro, string procedimentos)
+        {
+            GlobalProjectSql globalProjectSql = new GlobalProjectSql();
+            return globalProjectSql.ListarProcedimentos(conex, parceiro, procedimentos);
+        }
+
+        internal DataTable ListarLocalidades(ConexaoMySql conex, string procedimentos)
+        {
+            GlobalProjectSql globalProjectSql = new GlobalProjectSql();
+            return globalProjectSql.ListarLocalidades(conex, procedimentos);
+        }
+
+        internal DataTable ListarParceiros(ConexaoMySql conex, string procedimentos, string localidade)
+        {
+            GlobalProjectSql globalProjectSql = new GlobalProjectSql();
+            return globalProjectSql.ListarParceiros(conex, procedimentos, localidade);
+        }
+
+        internal DataTable ListarParceiros(ConexaoMySql conex)
+        {
+            GlobalProjectSql globalProjectSql = new GlobalProjectSql();
+            return globalProjectSql.ListarParceiros(conex);
+        }
+
+        internal DataTable ListarUnidades(ConexaoMySql conex)
+        {
+            GlobalProjectSql globalProjectSql = new GlobalProjectSql();
+            return globalProjectSql.ListarUnidades(conex);
+        }
+
+        internal int GravarUsuario(ConexaoMySql conex, int usuId, int idCadGobal, int unidade, string nome, string sexo, string CPF, string nascimento, int telefoneDDD, int telefone, int celularDDD, int celular, int cep, string rua, int numero, string complemento, string bairro, string cidade, string UF, string email, string senha)
+        {
+            GlobalProjectSql globalProjectSql = new GlobalProjectSql();
+            return globalProjectSql.GravarUsuario(conex, usuId, idCadGobal, unidade, nome, sexo, CPF, nascimento, telefoneDDD, telefone, celularDDD, celular, cep, rua, numero, complemento, bairro, cidade, UF, email, senha);
+        }
+
+        internal int GravarGrupo(ConexaoMySql conex, int gruId, int parceiro, string descricao, int qtdProcedimentos, int usuId)
+        {
+            GlobalProjectSql globalProjectSql = new GlobalProjectSql();
+            return globalProjectSql.GravarGrupo(conex, gruId, parceiro, descricao, qtdProcedimentos, usuId);
+        }
+
+        internal void GravarProcedimentosGrupo(ConexaoMySql conex, int gruId, int procedimento)
+        {
+            GlobalProjectSql globalProjectSql = new GlobalProjectSql();
+            globalProjectSql.GravarProcedimentosGrupo(conex, gruId, procedimento);
+        }
+
+        internal int GravarGuia(ConexaoMySql conex, int unidade, int parceiro, int associado, decimal valorTotal)
+        {
+            GlobalProjectBll globalProjectBll = new GlobalProjectBll();
+            return GlobalProjectSql.GravarGuia(conex, unidade, parceiro, associado, valorTotal);
+        }
+
+        internal int GravarItem(ConexaoMySql conex, int idGuia, int procedimento, decimal valor)
+        {
+            GlobalProjectSql globalProjectSql = new GlobalProjectSql();
+            return globalProjectSql.GravarItem(conex, idGuia, procedimento, valor);
+        }
+
+        internal DataTable ListarAgendamentos(ConexaoMySql conex, int usuId)
+        {
+            GlobalProjectSql globalProjectSql = new GlobalProjectSql();
+            return globalProjectSql.ListarAgendamentos(conex, usuId);
+        }
+
+        internal int GravarCartao(ConexaoMySql conex, int usuId, string nome, Int64 numero, int mes, int ano, string bandeira)
+        {
+            GlobalProjectSql globalProjectSql = new GlobalProjectSql();
+            return globalProjectSql.GravarCartao(conex, usuId, nome, numero, mes, ano, bandeira);
+        }
+
+        internal void ExcluirCartao(ConexaoMySql conex, int carId)
+        {
+            GlobalProjectSql globalProjectSql = new GlobalProjectSql();
+            globalProjectSql.ExcluirCartao(conex, carId);
+        }
+
+        internal DataTable ListarCartao(ConexaoMySql conex, int usuId)
+        {
+            GlobalProjectSql globalProjectSql = new GlobalProjectSql();
+            return globalProjectSql.ListarCartao(conex, usuId);
+        }
+
+        //private bool CaixaAbertoUsuario(classConexao conex, int cxaUsuAtual)//carregar ddlCaixa 
+        //{
+        //    CaixaAditivoSql arquivoSql = new CaixaAditivoSql();
+        //    return !arquivoSql.CaixaAberto(conex, cxaUsuAtual).Rows.Count.Equals(0);
+        //}
+    }
+}
---
+++ WebService/GlobalProjectWS/GlobalProjectWS/GlobalProjectWS.asmx.cs
@@ -0,0 +1,324 @@
+﻿using GlobalProjectWS.BussinesLogic;
+using System;
+using System.Data;
+using System.Web.Services;
+
+namespace GlobalProjectWS
+{
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(true)]
+    // Para permitir que esse serviço da web seja chamado a partir do script, usando ASP.NET AJAX, remova os comentários da linha a seguir. 
+    [System.Web.Script.Services.ScriptService]
+    public class GlobalProjectWS : System.Web.Services.WebService
+    {
+
+        [WebMethod]
+        public string HelloWorld()
+        {
+            return "Olá, Mundo";
+        }
+
+        [WebMethod]
+        public DataTable DadosUsuario(string login, string senha)
+        {
+            ConexaoMySql conex = new ConexaoMySql();
+
+            if (conex.Open())
+            {
+                GlobalProjectBll globalProjectBll = new GlobalProjectBll();
+                return (globalProjectBll.DadosUsuario(conex, login, senha));
+            }
+
+            return null;
+        }
+
+        [WebMethod]
+        public DataTable BuscarUsuario(string cpf)
+        {
+            ConexaoMySql conex = new ConexaoMySql();
+
+            if (conex.Open())
+            {
+                GlobalProjectBll globalProjectBll = new GlobalProjectBll();
+                return (globalProjectBll.DadosUsuario(conex, cpf));
+            }
+
+            return null;
+        }
+
+        [WebMethod]
+        public string Login(string login, string senha)
+        {
+            ConexaoMySql conex = new ConexaoMySql();
+
+            if (conex.Open())
+            {
+                GlobalProjectBll globalProjectBll = new GlobalProjectBll();
+                return (globalProjectBll.Login(conex, login, senha));
+            }
+
+            return null;
+        }
+
+        [WebMethod]
+        public DataTable ListarProcedimentos(string keyword)
+        {
+            ConexaoMySql conex = new ConexaoMySql();
+
+            if (conex.Open())
+            {
+                GlobalProjectBll globalProjectBll = new GlobalProjectBll();
+                return (globalProjectBll.ListarProcedimentos(conex, keyword));
+            }
+
+            return null;
+        }
+
+        [WebMethod]
+        public DataTable ListarGrupos()
+        {
+            ConexaoMySql conex = new ConexaoMySql();
+
+            if (conex.Open())
+            {
+                GlobalProjectBll globalProjectBll = new GlobalProjectBll();
+                return (globalProjectBll.ListarGrupos(conex));
+            }
+
+            return null;
+        }
+
+        [WebMethod]
+        public DataTable ListarGrupoId(int gruId)
+        {
+            ConexaoMySql conex = new ConexaoMySql();
+
+            if (conex.Open())
+            {
+                GlobalProjectBll globalProjectBll = new GlobalProjectBll();
+                return (globalProjectBll.ListarGrupos(conex, gruId));
+            }
+
+            return null;
+        }
+
+        [WebMethod]
+        public void ExcluirGrupo(int gruId)
+        {
+            ConexaoMySql conex = new ConexaoMySql();
+
+            if (conex.Open())
+            {
+                GlobalProjectBll globalProjectBll = new GlobalProjectBll();
+                globalProjectBll.ExcluirGrupo(conex, gruId);
+            }
+        }
+
+        [WebMethod]
+        public DataTable ListarProcedimentosParc(int parceiro)
+        {
+            ConexaoMySql conex = new ConexaoMySql();
+
+            if (conex.Open())
+            {
+                GlobalProjectBll globalProjectBll = new GlobalProjectBll();
+                return (globalProjectBll.ListarProcedimentos(conex, parceiro));
+            }
+
+            return null;
+        }
+
+        [WebMethod]
+        public DataTable ListarLocalidades(string procedimento)
+        {
+            ConexaoMySql conex = new ConexaoMySql();
+
+            if (conex.Open())
+            {
+                GlobalProjectBll globalProjectBll = new GlobalProjectBll();
+                return (globalProjectBll.ListarLocalidades(conex, procedimento));
+            }
+
+            return null;
+        }
+
+        [WebMethod]
+        public DataTable ListarParceiros(string procedimento, string localidade)
+        {
+            ConexaoMySql conex = new ConexaoMySql();
+
+            if (conex.Open())
+            {
+                GlobalProjectBll globalProjectBll = new GlobalProjectBll();
+                return (globalProjectBll.ListarParceiros(conex, procedimento, localidade));
+            }
+
+            return null;
+        }
+
+        [WebMethod]
+        public DataTable ListarParceirosAll()
+        {
+            ConexaoMySql conex = new ConexaoMySql();
+
+            if (conex.Open())
+            {
+                GlobalProjectBll globalProjectBll = new GlobalProjectBll();
+                return (globalProjectBll.ListarParceiros(conex));
+            }
+
+            return null;
+        }
+
+        [WebMethod]
+        public DataTable ListarUnidades()
+        {
+            ConexaoMySql conex = new ConexaoMySql();
+
+            if (conex.Open())
+            {
+                GlobalProjectBll globalProjectBll = new GlobalProjectBll();
+                return (globalProjectBll.ListarUnidades(conex));
+            }
+
+            return null;
+        }
+
+        [WebMethod]
+        public int GravarUsuario(int usuId, int idCadGobal, int unidade, string nome, string sexo, string CPF, string nascimento, int telefoneDDD, int telefone, int celularDDD, int celular, int cep, string rua, int numero, string complemento, string bairro, string cidade, string UF, string email, string senha)
+        {
+            ConexaoMySql conex = new ConexaoMySql();
+
+            if (conex.Open())
+            {
+                GlobalProjectBll globalProjectBll = new GlobalProjectBll();
+                return (globalProjectBll.GravarUsuario(conex, usuId, idCadGobal, unidade, nome, sexo, CPF, nascimento, telefoneDDD, telefone, celularDDD, celular, cep, rua, numero, complemento, bairro, cidade, UF, email, senha));
+            }
+
+            return 0;
+        }
+
+        [WebMethod]
+        public DataTable ListarProcedimentosGrupo(int parceiro, string procedimentos)
+        {
+            ConexaoMySql conex = new ConexaoMySql();
+
+            if (conex.Open())
+            {
+                GlobalProjectBll globalProjectBll = new GlobalProjectBll();
+                return (globalProjectBll.ListarProcedimentos(conex, parceiro, procedimentos));
+            }
+
+            return null;
+        }
+
+        [WebMethod]
+        public int GravarGrupo(int gruId, int parceiro, string descricao, int qtdProcedimentos, int usuId)
+        {
+            ConexaoMySql conex = new ConexaoMySql();
+
+            if (conex.Open())
+            {
+                GlobalProjectBll globalProjectBll = new GlobalProjectBll();
+                return globalProjectBll.GravarGrupo(conex, gruId, parceiro, descricao, qtdProcedimentos, usuId);
+            }
+
+            return 0;
+        }
+
+        [WebMethod]
+        public void GravarProcedimentosGrupo(int gruId, int procedimento)
+        {
+            ConexaoMySql conex = new ConexaoMySql();
+
+            if (conex.Open())
+            {
+                GlobalProjectBll globalProjectBll = new GlobalProjectBll();
+                globalProjectBll.GravarProcedimentosGrupo(conex, gruId, procedimento);
+            }
+        }
+
+        [WebMethod]
+        public int GravarGuia(int unidade, int parceiro, int associado, decimal valorTotal)
+        {
+            ConexaoMySql conex = new ConexaoMySql();
+
+            if (conex.Open())
+            {
+                GlobalProjectBll globalProjectBll = new GlobalProjectBll();
+                return globalProjectBll.GravarGuia(conex, unidade, parceiro, associado, valorTotal);
+            }
+
+            return 0;
+        }
+
+        [WebMethod]
+        public int GravarItem(int idGuia, int procedimento, decimal valor)
+        {
+            ConexaoMySql conex = new ConexaoMySql();
+
+            if (conex.Open())
+            {
+                GlobalProjectBll globalProjectBll = new GlobalProjectBll();
+                return globalProjectBll.GravarItem(conex, idGuia, procedimento, valor);
+            }
+
+            return 0;
+        }
+
+        [WebMethod]
+        public DataTable ListarAgendamentos(int usuId)
+        {
+            ConexaoMySql conex = new ConexaoMySql();
+
+            if (conex.Open())
+            {
+                GlobalProjectBll globalProjectBll = new GlobalProjectBll();
+                return (globalProjectBll.ListarAgendamentos(conex, usuId));
+            }
+
+            return null;
+        }
+
+        [WebMethod]
+        public int GravarCartao(int usuId, string nome, Int64 numero, int mes, int ano, string bandeira)
+        {
+            ConexaoMySql conex = new ConexaoMySql();
+
+            if (conex.Open())
+            {
+                GlobalProjectBll globalProjectBll = new GlobalProjectBll();
+                return globalProjectBll.GravarCartao(conex, usuId, nome, numero, mes, ano, bandeira);
+            }
+
+            return 0;
+        }
+
+        [WebMethod]
+        public void ExcluirCartao(int carId)
+        {
+            ConexaoMySql conex = new ConexaoMySql();
+
+            if (conex.Open())
+            {
+                GlobalProjectBll globalProjectBll = new GlobalProjectBll();
+                globalProjectBll.ExcluirCartao(conex, carId);
+            }
+        }
+
+        [WebMethod]
+        public DataTable ListarCartao(int usuId)
+        {
+            ConexaoMySql conex = new ConexaoMySql();
+
+            if (conex.Open())
+            {
+                GlobalProjectBll globalProjectBll = new GlobalProjectBll();
+                return (globalProjectBll.ListarCartao(conex, usuId));
+            }
+
+            return null;
+        }
+    }
+}
